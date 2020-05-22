ef9e235dc611391f64a3f9b0ffc337ee037cf153
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-22 00:06:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
     array.each_with_index do |element, index|
-    puts index 
+    puts index.to_string + ". " + element 
   end
   
 end
